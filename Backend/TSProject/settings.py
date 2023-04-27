512d35dc12411db5e0bee65c09ab7101549c103b
Josh Sawyer <thejoshsawyer@gmail.com>
2023-04-27 01:36:31 -0700

    Updated settings to access SQL database, requires .env to get private information that allows access to the database

--- Backend/TSProject/settings.py
+++ Backend/TSProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'TSProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
     }
 }
 
