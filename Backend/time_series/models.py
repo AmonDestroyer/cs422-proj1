6c9be6ddae019523cf64fc3244627b70d4ce2c7e
Josh Sawyer <thejoshsawyer@gmail.com>
2023-04-27 01:25:47 -0700

    Added the ability to upload JSON to database

--- Backend/time_series/models.py
+++ Backend/time_series/models.py
@@ -11,7 +11,7 @@ class TimeUnit(models.Model):
 
 
 class TS_Set(models.Model):
-  set_id = models.IntegerField(primary_key=True)
+  set_id = models.IntegerField(primary_key=True, db_column='set_id')
   set_name = models.CharField(max_length=200, null=True)
   description = models.CharField(max_length=1000, null=True)
   vector_size = models.IntegerField(null=True)
@@ -20,6 +20,8 @@ class TS_Set(models.Model):
   num_ts = models.IntegerField(null=True)
   time_unit = models.ForeignKey(TimeUnit, on_delete=models.DO_NOTHING, db_column='Time Unit_tu_id')
 
+  class Meta:
+    db_table = 'TS_Set'
 
 class TimeSeries(models.Model):
   ts_id = models.IntegerField(primary_key=True)
@@ -30,7 +32,7 @@ class TimeSeries(models.Model):
   vector_size = models.IntegerField(null=True)
   length = models.IntegerField(null=True)
   sampling_period = models.IntegerField(null=True)
-  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='TS_Set_set_id')
+  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='set_id')
   
   class Meta:
     db_table = 'Time Series'
@@ -39,53 +41,65 @@ class TimeSeries(models.Model):
 class Keyword(models.Model):
   keyword_id = models.IntegerField(primary_key=True)
   keyword = models.CharField(max_length=100, null=True)
+  
+  class Meta:
+    db_table = 'Keyword'
 
 
 class Contributor(models.Model):
   contrib_id = models.IntegerField(primary_key=True)
   contrib_fname = models.CharField(max_length=100, null=True)
   contrib_lname = models.CharField(max_length=100, null=True)
+  
+  class Meta:
+    db_table = 'Contributor'
 
 
 class Domain(models.Model):
   domain_id = models.IntegerField(primary_key=True)
   domain_name = models.CharField(max_length=1000, null=True)
+  
+  class Meta:
+    db_table = 'Domain'
 
 
 class Paper(models.Model):
   paper_id = models.IntegerField(primary_key=True)
   paper_reference = models.CharField(max_length=1000, null=True)
   paper_link = models.CharField(max_length=1000, null=True)
+  
+  class Meta:
+    db_table = 'Paper'
 
 
 class SetKeyword_Join(models.Model):
-  keyword_id = models.ForeignKey(Keyword, on_delete=models.DO_NOTHING, db_column='Keyword_keyword_id')
-  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='TS_Set_set_id')
+  keyword_id = models.ForeignKey(Keyword, on_delete=models.DO_NOTHING, db_column='keyword_id')
+  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='set_id')
   
   class Meta:
     db_table = 'Set-Keyword_Join'
 
 
 class SetContributor_Join(models.Model):
-  contrib_id = models.ForeignKey(Contributor, on_delete=models.DO_NOTHING, db_column='Contributor_contrib_id')
-  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='TS_Set_set_id')
+  contrib_id = models.ForeignKey(Contributor, on_delete=models.DO_NOTHING, db_column='contrib_id')
+  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='set_id')
   
   class Meta:
     db_table = 'Set-Contributor_Join'
     
 
 class SetPaper_Join(models.Model):
-  paper_id = models.ForeignKey(Paper, on_delete=models.DO_NOTHING, db_column='Paper_paper_id')
-  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='TS_Set_set_id')
+  paper_id = models.ForeignKey(Paper, on_delete=models.DO_NOTHING, db_column='paper_id')
+  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='set_id')
   
   class Meta:
     db_table = 'Set-Paper_Join'
 
 
 class TimeseriesKeyword_Join(models.Model):
-  ts_id = models.ForeignKey(TimeSeries, on_delete=models.DO_NOTHING, db_column='Time Series_ts_id')
-  keyword_id = models.ForeignKey(Keyword, on_delete=models.DO_NOTHING, db_column='Keyword_keyword_id')
-  
+  keyword_id = models.ForeignKey(Keyword, on_delete=models.DO_NOTHING, db_column='keyword_id')
+  ts_id = models.ForeignKey(TimeSeries, on_delete=models.DO_NOTHING, db_column='ts_id')
+
   class Meta:
     db_table = 'Timeseries-Keyword_Join'
     
@@ -96,18 +110,20 @@ class TS_Measurement(models.Model):
   y_val = models.FloatField(null=True)
   ts_id = models.ForeignKey(TimeSeries, on_delete=models.DO_NOTHING, db_column='Time Series_ts_id')
   
+  class Meta:
+    db_table = 'TS_Measurement'
 
 class TimeseriesDomain_Join(models.Model):
-  domain_id = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, db_column='Domain_domain_id')
-  ts_id = models.ForeignKey(TimeSeries, on_delete=models.DO_NOTHING, db_column='Time Series_ts_id')
+  domain_id = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, db_column='domain_id')
+  ts_id = models.ForeignKey(TimeSeries, on_delete=models.DO_NOTHING, db_column='ts_id')
   
   class Meta:
     db_table = 'Timeseries-Domain_Join'
 
 
 class TimeseriesSetDomain_Join(models.Model):
-  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='TS_Set_set_id')
-  domain_id = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, db_column='Domain_domain_id')
+  domain_id = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, db_column='domain_id')
+  set_id = models.ForeignKey(TS_Set, on_delete=models.DO_NOTHING, db_column='set_id')
   
   class Meta:
     db_table = 'TimeseriesSet-Domain_Join'
--- Backend/time_series/upload.py
+++ Backend/time_series/upload.py
@@ -1,4 +1,110 @@
 import json
+from .models import *
+from django.db import transaction
 
-def upload_file(request_body):
-  data = json.loads(request_body)
+
+ 
+def upload_train_test_to_db(request_body):
+  id = 40 # Temporary for now - anything related to 'id' will be changed later
+  
+  data = json.loads(request_body) # Load JSON into python dictionary (data)
+  set_meta = data['setMeta'] # setMeta contains all the set's metadata - set_meta is the dictionary for that metadata
+  ts_meta = data['seriesMeta'] # tsMeta contains the time series's metadata - ts_meta is the dictionary for that metadata
+  series_data = data['seriesData'] # seriesData contains the time series's data - series_data is the dictionary for that data
+  
+  try:
+    with transaction.atomic():
+      # Create TimeUnit object using TimeUnit object and data from set_meta - add to the database
+      # Save the instance of this object to time_unit_instance which will be used for other objects
+      time_unit_instance = TimeUnit.objects.create(tu_id=id, unit_name='min') 
+      
+      # Create TS_Set object using TS_Set object and data from set_meta
+      # objects.create() creates the object and then saves it to the database
+      # ts_set is an instance of the TS_Set class that is in the database
+      
+      
+      ts_set = TS_Set.objects.create(
+        set_id=id,
+        set_name=set_meta['TS Set Name'],
+        description=set_meta['Description'],
+        vector_size=set_meta['Vector Size'],
+        min_length=set_meta['Min Length'],
+        max_length=set_meta['Max Length'],
+        num_ts=set_meta['Number of TS in the Set'],
+        time_unit=time_unit_instance,
+      )
+      
+      
+      # Create TimeSeries object using TimeSeries object and data from ts_meta
+      # objects.create() creates the object and then saves it to the database
+      # ts_timeseries is an instance of the TimeSeries class that is in the database
+      timeseries = TimeSeries.objects.create(
+        ts_id=id,
+        ts_name=ts_meta['TS Name'],
+        description=ts_meta['Description'],
+        y_unit=ts_meta['Units'],
+        scalar_vector=ts_meta['Scalar/Vector'],
+        vector_size=ts_meta['Vector Size'],
+        length=ts_meta['Length'],
+        sampling_period=ts_meta['Sampling Period'],
+        set_id=ts_set
+      )
+      
+      
+      # loop through all the data in series_data and create a TS_Measurement object for each point
+      # each TS_Measurement object will then be created as a row in the Table TS_Measurement in the Database
+      # each TS_Measurement object ts_id links it to the timeseries that was created above
+      for _, temp in series_data.items():
+        TS_Measurement.objects.create(
+          tsm_id=id,
+          x_val=temp,
+          ts_id=timeseries # links the TS_Measurement object to the TimeSeries object (row in table) that was created above
+        )
+        id=id + 1 
+      
+      # Additional meta data for TS_Set that will be joined
+      set_domain = Domain.objects.create(domain_id=id, domain_name=set_meta['Application Domain(s)'])
+      set_keyword = Keyword.objects.create(keyword_id=id, keyword=set_meta['Keywords'])
+      contributor = Contributor.objects.create(contrib_id=id, contrib_fname=set_meta['Contributors']) # Needs to be fixed later (need to split first and last name)
+      paper = Paper.objects.create(paper_id=id, paper_reference=set_meta['Related Paper Reference(s)'], paper_link=set_meta['Related Paper Link'])
+      
+      id = id + 1 
+      # Additional meta data for TimeSeries that will be joined
+      series_domain = Domain.objects.create(domain_id=id, domain_name=ts_meta['Domain(s)'])
+      series_keyword = Keyword.objects.create(keyword_id=id, keyword=ts_meta['Keywords'])
+      
+      
+      # Set joiners
+      TimeseriesSetDomain_Join.objects.create(domain_id=set_domain, set_id=ts_set)
+      SetKeyword_Join.objects.create(keyword_id=set_keyword, set_id=ts_set)
+      SetContributor_Join.objects.create(contrib_id=contributor, set_id=ts_set)
+      SetPaper_Join.objects.create(paper_id=paper, set_id=ts_set)
+      
+      # TimeSeries joiners
+      TimeseriesDomain_Join.objects.create(domain_id=series_domain, ts_id=timeseries)
+      TimeseriesKeyword_Join.objects.create(keyword_id=series_keyword, ts_id=timeseries)
+      
+      id = id + 1
+      return 1, 'Success'
+  except Exception as e:
+    return 0, e
+    
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+  
+  
+  
+  
+  
+  
