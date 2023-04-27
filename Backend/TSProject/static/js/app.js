2d18317a7e746efde94e80e33b9dc070834a5e1f
Luke99326 <lukegscribner@gmail.com>
2023-04-27 16:50:00 -0700

    Added support for .xlsx and .xls, added message saying whether or not database input was successful

--- Backend/TSProject/static/js/app.js
+++ Backend/TSProject/static/js/app.js
@@ -10,7 +10,73 @@ const sendFile = (file) => {
         method: 'POST',
         body: file
     })
-    .then(response => console.log(response))
+    .then(response => {
+        console.log(response)
+        if (response.status === 200) {
+            document.querySelector('#upSuccess').style.display = "inline";
+        } else {
+            document.querySelector('#upError').style.display = "inline";
+        }
+        document.getElementById('myFile').value = ""
+    })
+    .catch(error => console.log(error))
+}
+
+
+const convXlsx = (file) => {
+    let reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = function() {
+        let data = reader.result;
+        let workbook = XLSX.read(data, {type: 'binary'});
+        workbook.SheetNames.forEach(function(sheetName) {
+            let XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 1});
+            console.log(XL_row_object[0][11]);
+
+            const setMeta = {
+                "TS Set Name": XL_row_object[0][0],
+                "Description": XL_row_object[0][1],
+                "Application Domain(s)": XL_row_object[0][2],
+                "Keywords": XL_row_object[0][3],
+                "Vector Size": XL_row_object[0][4],
+                "Min Length": XL_row_object[0][5],
+                "Max Length": XL_row_object[0][6],
+                "Number of TS in the Set": XL_row_object[0][7],
+                "Start Datetime": XL_row_object[0][8],
+                "Contributors": XL_row_object[0][9],
+                "Related Paper Reference(s)": XL_row_object[0][10],
+                "Related Paper Link": XL_row_object[0][11]
+            }
+
+            const seriesMeta = {
+                "TS Name": XL_row_object[10][0],
+                "Description": XL_row_object[2][0],
+                "Domain(s)": XL_row_object[3][0],
+                "Units": XL_row_object[4][0],
+                "Keywords": XL_row_object[5][0],
+                "Scalar/Vector": XL_row_object[6][0],
+                "Vector Size": XL_row_object[7][0],
+                "Length": XL_row_object[8][0],
+                "Sampling Period": XL_row_object[9][0]
+            }
+
+            let seriesData = {}
+            for (let i = 11; i < XL_row_object.length; i++) {
+                seriesData["point" + String(i-11)] = XL_row_object[i][0]
+            }
+            
+
+            const fullJson = {
+                "setMeta": setMeta,
+                "seriesMeta": seriesMeta,
+                "seriesData": seriesData
+            }
+            console.log(fullJson);
+
+            sendFile(JSON.stringify(fullJson))
+
+        })
+    }
 }
 
 
@@ -78,18 +144,34 @@ function fileValidation(fileInput){
     // Gets the file extension
     const fileExtension = filePath.substring(filePath.indexOf('.') + 1).toLowerCase();
 
-    //Check for file extension validation
-    if (fileExtension === 'txt' || fileExtension === 'csv' || fileExtension === 'xlsx'
-     || fileExtension === 'excel' || fileExtension === "JSON") {
-        console.log("File accepted")
-    }
-    else{
-        fileInput.value = "";
-        alert("Invalid file type!")
-    }
+
+    // DO THE FILE VALIDATION TO CHECK THAT ALL REQUIRED FIELD ARE FILLED IN
 
 
-    if (fileExtension === "csv") {
-        convCsv(fileInput.files[0])
+    switch (fileExtension) {
+        case "txt":
+            convCsv(fileInput.files[0])
+            break;
+
+        case "json":
+            sendFile(JSON.stringify(fileInput.files[0]))
+            break;
+
+        case "csv":
+            convCsv(fileInput.files[0])
+            break;
+
+        case "xlsx":
+            convXlsx(fileInput.files[0])
+            break;
+
+        case "xls":
+            convXlsx(fileInput.files[0])
+            break;
+
+
+        default:
+            alert("Invalid file type")
+            break;
     }
 }
