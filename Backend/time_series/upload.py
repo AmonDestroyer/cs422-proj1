eb776c60b245e7601a9362a51d7336e90dc4c31e
Josh Sawyer <thejoshsawyer@gmail.com>
2023-04-29 14:13:14 -0700

    Updated uploading for train and test TS sets, solution TS set still needs to be implemented

--- Backend/time_series/upload.py
+++ Backend/time_series/upload.py
@@ -1,13 +1,12 @@
-import json
 from .models import *
 from django.db import transaction
 
-
+# Constants for set_type
+TRAIN = 'train' 
+SOLUTION = 'solution'
+TEST = 'test'
  
-def upload_train_test_to_db(request_body):
-  id = 40 # Temporary for now - anything related to 'id' will be changed later
-  
-  data = json.loads(request_body) # Load JSON into python dictionary (data)
+def upload_set(data, set_type, train_set=None):
   set_meta = data['setMeta'] # setMeta contains all the set's metadata - set_meta is the dictionary for that metadata
   ts_meta = data['seriesMeta'] # tsMeta contains the time series's metadata - ts_meta is the dictionary for that metadata
   series_data = data['seriesData'] # seriesData contains the time series's data - series_data is the dictionary for that data
@@ -18,43 +17,18 @@ def upload_train_test_to_db(request_body):
       # Save the instance of this object to time_unit_instance which will be used for other objects
       time_unit = TimeUnit.objects.create(unit_name='min') 
       
-      # INCOMPLETE #
-      # Create SetType object using SetType object and data from set_meta - add to the database
-      # This tells what type of set was uploaded (train, test)
-      set_type = SetType.objects.create(set_type='TEMP') # Need to change temp to set_meta['_']
-      # INCOMPLETE # 
+      # This tells what type of set was uploaded (train, test, solution)
+      set_type_obj = SetType.objects.create(set_type=set_type) 
       
       # Create TS_Set object using TS_Set object and data from set_meta
       # objects.create() creates the object and then saves it to the database
       # ts_set is an instance of the TS_Set class that is in the database
-      ts_set = TS_Set.objects.create(
-        set_name=set_meta['TS Set Name'],
-        description=set_meta['Description'],
-        vector_size=set_meta['Vector Size'],
-        min_length=set_meta['Min Length'],
-        max_length=set_meta['Max Length'],
-        num_ts=set_meta['Number of TS in the Set'],
-        start_datetime=set_meta['Start Datetime'],
-        error=None, # No error is sent when uploading train/test sets
-        tu_id=time_unit,
-        set_type_id=set_type
-      )
-      
+      ts_set = create_ts_set(set_meta, time_unit, set_type_obj)
       
       # Create TimeSeries object using TimeSeries object and data from ts_meta
       # objects.create() creates the object and then saves it to the database
       # ts_timeseries is an instance of the TimeSeries class that is in the database
-      timeseries = TimeSeries.objects.create(
-        ts_name=ts_meta['TS Name'],
-        description=ts_meta['Description'],
-        y_unit=ts_meta['Units'],
-        scalar_vector=ts_meta['Scalar/Vector'],
-        vector_size=ts_meta['Vector Size'],
-        length=ts_meta['Length'],
-        sampling_period=ts_meta['Sampling Period'],
-        set_id=ts_set
-      )
-      
+      timeseries = create_time_series(ts_meta, ts_set)
       
       # loop through all the data in series_data and create a TS_Measurement object for each point
       # each TS_Measurement object will then be created as a row in the Table TS_Measurement in the Database
@@ -65,18 +39,15 @@ def upload_train_test_to_db(request_body):
           ts_id=timeseries # links the TS_Measurement object to the TimeSeries object (row in table) that was created above
         )
       
-      
       # Additional meta data for TS_Set that will be joined
       set_domain = Domain.objects.create(domain_name=set_meta['Application Domain(s)'])
       set_keyword = Keyword.objects.create(keyword=set_meta['Keywords'])
       contributor = Contributor.objects.create(contrib_fname=set_meta['Contributors']) # Needs to be fixed later (need to split first and last name)
       paper = Paper.objects.create(paper_reference=set_meta['Related Paper Reference(s)'], paper_link=set_meta['Related Paper Link'])
       
-      id = id + 1 
       # Additional meta data for TimeSeries that will be joined
       series_domain = Domain.objects.create(domain_name=ts_meta['Domain(s)'])
       series_keyword = Keyword.objects.create(keyword=ts_meta['Keywords'])
-      
       
       # Set joiners
       TimeseriesSetDomain_Join.objects.create(domain_id=set_domain, set_id=ts_set)
@@ -88,18 +59,61 @@ def upload_train_test_to_db(request_body):
       TimeseriesDomain_Join.objects.create(domain_id=series_domain, ts_id=timeseries)
       TimeseriesKeyword_Join.objects.create(keyword_id=series_keyword, ts_id=timeseries)
       
-      return 1, 'Success'
+      if (set_type == TRAIN):
+        # 1 indicates success
+        return 1, ts_set # TS set needs to be returned so that we can link
+      
+      else: # if set_type is not "train" then we want to link to the train set
+        # Need to change this to the id of the train set that this test set is associated with
+        # train_id = '1' # Most likely will need to be data['train_id'] or something like that
+        # Get TS_Set with train_id from table since this will be needed to link the two sets
+        # train_ts_set = TS_Set.objects.get(set_id=train_id)
+        
+        # Create TestTrainingSolution_Join object using TestTrainingSolution_Join object
+        # other_set is the set we just created (see line 30) and we link this to the train set
+        TestTrainingSolution_Join.objects.create(training_set_id=train_set, other_set=ts_set)
+        
+        return 1, ts_set
+ 
   except Exception as e:
     return 0, e
-    
+  
+  
+ 
+def create_ts_set(set_meta, time_unit, set_type_obj):
+  error = None
+  if (set_type_obj.set_type == SOLUTION):
+    error = 1 # Need to calculate error here
+  
+  ts_set = TS_Set.objects.create(
+        set_name=set_meta['TS Set Name'],
+        description=set_meta['Description'],
+        vector_size=set_meta['Vector Size'],
+        min_length=set_meta['Min Length'],
+        max_length=set_meta['Max Length'],
+        num_ts=set_meta['Number of TS in the Set'],
+        start_datetime=set_meta['Start Datetime'],
+        error=error, # No error is sent when uploading train/test sets
+        tu_id=time_unit,
+        set_type_id=set_type_obj
+      ) 
+  
+  return ts_set
 
+# Incomplete? May need to add a loop to create multiple TimeSeries objects
+def create_time_series(ts_meta, ts_set):
+  timeseries = TimeSeries.objects.create(
+        ts_name=ts_meta['TS Name'],
+        description=ts_meta['Description'],
+        y_unit=ts_meta['Units'],
+        scalar_vector=ts_meta['Scalar/Vector'],
+        vector_size=ts_meta['Vector Size'],
+        length=ts_meta['Length'],
+        sampling_period=ts_meta['Sampling Period'],
+        set_id=ts_set
+      )
   
-  
-  
-  
-  
-  
-  
+  return timeseries
   
   
   
--- Backend/time_series/views.py
+++ Backend/time_series/views.py
@@ -1,12 +1,15 @@
-# Request Handler
 from django.views.decorators.csrf import csrf_exempt
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .upload import upload_train_test_to_db
+from .upload import upload_set, TRAIN, SOLUTION, TEST
 from .download import get_train_data, get_solutions
+from .calculate import calculate_error
 
+import json
+
+### Render page request handlers ###
 def redirect_home(request):
   return redirect('home/')
 
@@ -26,28 +29,65 @@ def login(request):
   return redirect('roles:login')
 
 
-# upload file (training + test data) - POST request handler
+### Database POST request handlers ###
+
+# upload training and test data
 @csrf_exempt
 def upload_data(request):
   if request.method == 'POST':
+    data_dict = json.loads(request.body) # Load JSON into python dictionary (data)
+    
+    # Check if a train and test set was uploaded
+    # If so, first upload train set
+    # uploading train set should return the object that was created
+    # then upload test set and pass in the train set object
+    if ('trainSet' in data_dict) and ('testSet' in data_dict):
+      print("Recieved train and test set")
 
+      # Upload train set
+      response = upload_set(data=data_dict['trainSet'], set_type=TRAIN)
+      if (response[0] == 0): # Failed to upload 
+        print(response[1])
+        return HttpResponse(response[1], status=400)
+      
+      else:
+        response = upload_set(data=data_dict['testSet'], set_type=TEST, train_set=response[1])
+        if (response[0] == 0):
+          print(response[1])
+          return HttpResponse(response[1], status=400)
+        else:
+          return HttpResponse('Success', status=200)
+    
+    # Check if a solution set was uploaded
+    # If so, calculate error then upload solution set
+    elif ('ProblemID' in data_dict):
+      print("Recieved problem set")
+      return HttpResponse('Not-Implemented', status=200)
+
+  else: # GET request (or any other) - redirect to home page since this is not a valid request
+    return redirect('../home/')
+    
+
+@csrf_exempt
+def upload_solution(request):
+  if request.method == 'POST':
     jsonStr = request.body
-    # print(jsonStr)
     
-    # call function to process uploaded file - send request.body to function
-    response = upload_train_test_to_db(jsonStr)
+    # Upload the solution set
+    # Need to make sure to pass in the ts_set_id of the train set
+    # request.body: {"ts set id": "num", "solution": "{data}"}
+    # might need to change around upload_set slightly so i can get both in
+    response = upload_set(jsonStr)
     
-    if response[0] == 0:
-      print(response[1])
-      return HttpResponse(response[1], status=400)
-    else:
-      # return render(request, 'contributor.html')
-      return HttpResponse(response[1], status=200)
-
-  else: # GET request - redirect to home page since this is not a valid request
+    # Response 1 will contain ts_set of the solution that was pushed
+    error = calculate_error(jsonStr["ts set id"], response[1])
+    
+    return HttpResponse('Success', status=200)
+  else: # GET request (or any other) - redirect to home page since this is not a valid request
     return redirect('../home/')
 
 
+### Database GET request handlers ###
 def train_data_download_request(request):
   response = get_train_data(request.body)
 
