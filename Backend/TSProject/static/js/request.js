bad614a4b686b25a1864e01fa6fdc10c3ec63121
sophiez <sophiez.89@outlook.com>
2023-05-01 07:54:16 -0700

    Display solution/problems

--- Backend/TSProject/static/js/request.js
+++ Backend/TSProject/static/js/request.js
@@ -1,22 +1,74 @@
-fetch('_get-train-data/', {
-    method: 'GET',
-    body: json
-})
-.then(response => {
-    console.log(response);
-    console.log("Sucessfully gotten data");
+// add an event listener to the refresh button
+const refreshButton = document.getElementById('refresh');
+const solutionBox = document.getElementById("Solution-Box");
+refreshButton.addEventListener('click', () => {
+    fetch('/_get-solution/')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        })
+        .then(data =>
+             {
+                console.log(data);
+            // do something with the retrieved data here
+            const problem_table = document.getElementById('problem-table');
+            for(const problem in data){
+                for(const solution in data[problem]){
+        
+                    // Problem Set Information
+                    const setName = data[problem][solution]["set name"];
+                    const contributorFName = data[problem][solution]["contributor first name"];
+                    const problemName = problem;
+                    
+                    // Insert a new problem row 
+                    const newProblem = problem_table.insertRow();
 
-})
-.then(data =>{
-    console.log(data);
-    // const problemDisplay = document.getElementById('table');
-    // data.forEach(item=>{
-    //     const row = document.createElement("tr");
-    //     const nameCell = document.createElement("td");
-    //     nameCell.textContent = item.namel; 
-    //     c
-    // })
-})
-.catch(error => {
-    console.error(error);
+                    // Insert cells for row information
+                    const SetNameCell = newProblem.insertCell();
+                    const ContriNameFCell = newProblem.insertCell();
+                    const problemCell = newProblem.insertCell();
+                    const solutionViewCell = newProblem.insertCell();
+
+                    // Create Text Node to add to cell
+                    const ContriNameFText = document.createTextNode(contributorFName);
+                    const setNameText = document.createTextNode(setName);
+                    const problemNameText = document.createTextNode(problemName);
+
+                    //Create solution button
+                    const solutionButton = document.createElement("button");
+                    solutionButton.innerHTML = "View Solution";
+                    // Add text to cell
+                    SetNameCell.appendChild(setNameText);
+                    ContriNameFCell.appendChild(ContriNameFText);
+                    problemCell.appendChild(problemNameText);
+                    solutionViewCell.appendChild(solutionButton);
+
+                    // Create Solution Div 
+                    const solutionDiv = document.createElement("div");
+                    solutionDiv.id = "solutionDiv";
+                    solutionBox.appendChild(solutionDiv);
+
+                    // Create exit solution div button 
+                    const exitButton = document.createElement("button"); 
+                    exitButton.innerText = "Exit";
+                    exitButton.id = "exit"
+                    solutionDiv.appendChild(exitButton);
+
+                    solutionButton.addEventListener("click", function(e){
+                        console.log("solution button was clicked"); 
+                        solutionDiv.style.disyplay = "block";
+                    });
+                    exitButton.addEventListener("click", function(e){
+                        console.log("Exit button was clicked"); 
+                        solutionDiv.style.display = "none";
+                    })
+                }
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
